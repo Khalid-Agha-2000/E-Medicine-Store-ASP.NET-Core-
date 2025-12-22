fbcce816d46913c7202af4d76a8065faccc45dd3
Khalid Agha <s.khalidagha2000@gmail.com>
2025-12-22 16:02:09 +0300

    Added basic cart page layout

--- Frontend/EMedicine-Frontend/src/components/users/MedicineDisplay.jsx
+++ Frontend/EMedicine-Frontend/src/components/users/MedicineDisplay.jsx
@@ -2,7 +2,7 @@ export default function MedicineDisplay() {
     return (
         <section className="py-5">
             <div className="container px-4 px-lg-5 mt-5">
-                <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-left">
                     <div className="col mb-5">
                         <div className="card h-100">
                             <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
@@ -13,7 +13,7 @@ export default function MedicineDisplay() {
                                 </div>
                             </div>
                             <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+                                <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">Add to Cart</a></div>
                             </div>
                         </div>
                     </div>
@@ -36,7 +36,7 @@ export default function MedicineDisplay() {
                                 </div>
                             </div>
                             <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
+                                <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">Add to Cart</a></div>
                             </div>
                         </div>
                     </div>
@@ -52,7 +52,7 @@ export default function MedicineDisplay() {
                                 </div>
                             </div>
                             <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
+                                <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">Add to Cart</a></div>
                             </div>
                         </div>
                     </div>
@@ -103,7 +103,7 @@ export default function MedicineDisplay() {
                                 </div>
                             </div>
                             <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+                                <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">Add to Cart</a></div>
                             </div>
                         </div>
                     </div>
