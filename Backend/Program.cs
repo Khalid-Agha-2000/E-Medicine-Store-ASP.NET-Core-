39dba9dc05a32157cc01aff7b60a6ad700ae7269
Khalid Agha <s.khalidagha2000@gmail.com>
2026-01-20 03:15:06 +0300

    Commented out migration related code to fix error on azure

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -101,10 +101,10 @@ app.UseAuthorization();
 // controllers
 app.MapControllers();
 
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
-}
+// using (var scope = app.Services.CreateScope())
+// {
+//     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+//     db.Database.Migrate();
+// }
 
 app.Run();
